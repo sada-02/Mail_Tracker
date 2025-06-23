e9918d99040ea84d54fbb4c78ee4409383005c34
sada-02 <kartiksarda.2005@gmail.com>
2025-06-23 17:27:00 +0530

    daily commits

--- X-feed/popup.js
+++ X-feed/popup.js
@@ -3,3 +3,27 @@ function loadPage(view) {
   console.log("Loading page:", view);
 
   chrome.storage.local.set({ currentPage: view });  // <-- set current view
+
+  fetch(`${view}.html`)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("container").innerHTML = html;
+
+      if (view === "login") import("./auth.js").then(m => m.initLogin());
+      if (view === "signup") import("./auth.js").then(m => m.initSignup());
+      if (view === "dashboard") import("./dashboard.js").then(m => m.initDashboard());
+    });
+}
+
+
+chrome.storage.local.get(["loggedInUser", "currentPage"], (data) => {
+  if (data.currentPage) {
+    loadPage(data.currentPage); // Load last requested page
+  } else {
+    if (data.loggedInUser) {
+      loadPage("dashboard");
+    } else {
+      loadPage("login");
+    }
+  }
+});
